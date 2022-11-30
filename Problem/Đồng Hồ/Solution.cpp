de7dc333954ffcb2acd6a9c495d8e1f0938a08e6
khuyenbd8bb <119473147+khuyenbd8bb@users.noreply.github.com>
2022-11-30 17:28:00 +0700

    Update Solution.cpp

--- Problem/Đồng Hồ/Solution.cpp
+++ Problem/Đồng Hồ/Solution.cpp
@@ -1,23 +1,25 @@
+// * ----------------- * //
 #include <bits/stdc++.h>
 #define boost ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define loop(a,b,c) for(int a = b; a <= c; a++)
 #define file freopen("???.inp","r",stdin); freopen("???.out","w",stdout);
 #define ll long long
 using namespace std;
-
 void solve() {
-	char c;
-	long double l, r;
+	char c; // Kí Tự Trống
+	long double l, r; // l = Giờ và r = Phút ( left, right )
 	while(cin >> l >> c >> r) {
-		if (l == 0 && r == 00) return;
+		if (l == 0 && r == 00) return; // Trường Hợp BREAK
+		// *** **** *** ** ** ** 
 		l = (ll)l%12 * 30;
-		l += (long double)r/60 * 30;
-		r = (ll)r%60 * 6;
-		cout << setprecision(3) << fixed;
-		long double res = abs(l - r);
-		res = min(res, 360 - res);
+		l += (long double)r/60 * 30; // Độ của kim giờ so với 12h đúng
+		r = (ll)r%60 * 6; // Độ của kim Phút so với 12h đúng
+		// *** ** ** ** ** ** **  *
+		cout << setprecision(3) << fixed; // set kết quả 3 chữ số thập phân
+		long double res = abs(l - r); 
+		res = min(res, 360 - res); // trường hợp đảm bảo yêu cầu <= 180*
 		cout << res << '\n';
-	}	
+	}	 
 }
 int main() { 
 	boost; 
